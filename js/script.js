45c9935a7592ee08779c91d80b633dfd3f92bbfd
Eugen <fredarkq18@gmail.com>
2021-12-01 16:16:30 +0200

    films

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log(1);
+const numberOfFilms = +prompt('How many films you watched?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+let lastFilm = prompt('Last film?', ''),
+    ocenka = prompt('Ocanka', ''),
+    lastFilm1 = prompt('Last film?', ''),
+    ocenka1 = prompt('Ocanka', '');
+personalMovieDB.movies[lastFilm] = ocenka;
+personalMovieDB.movies[lastFilm1] = ocenka1;
+console.log(personalMovieDB.movies);
