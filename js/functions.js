4480b8b168cf00cd3c892f214c08736e60e0e064
Eugen <fredarkq18@gmail.com>
2021-12-02 17:06:35 +0200

    functions and dii nad strokami

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+"use strict";
+const calc = (a, b) => a + b;
+
+
+
+const shos = (a, b) => {
+    console.log('1');
+    return a + b;
+};
+
+
+const str = "test";
+const arr = [1, 2, 4];
+
+console.log(str.length);
