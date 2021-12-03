39b1e277b637cb527545389df432848ec90eb259
Eugen <fredarkq18@gmail.com>
2021-12-03 16:40:40 +0200

    practise#3

--- js/functions.js
+++ js/functions.js
@@ -9,7 +9,56 @@ const shos = (a, b) => {
 };
 
 
-const str = "test";
+const str = "tEst";
 const arr = [1, 2, 4];
 
 console.log(str.length);
+console.log(str.toLowerCase());
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
+
+const logg = 'Hello world';
+console.log(logg.slice(-11, -5));
+
+const logg = 'Hello world';
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+
+
+function qqq(a,b) {
+    let result;
+    result = a % b;
+    if (result == false){
+       return console.log("Delitsa");
+    } else {
+       return console.log("Delitsa s ostatkom");
+    }
+}
+qqq(9,3);
+let besult = 1;
+let brr = [2, 3, 4, 5, 6];
+for (let i = 0; i < brr.length; i++) {
+    besult = besult * brr[i];
+}
+console.log(besult);
+
+let a = 1;
+while (a <= 100) {
+    console.log(a);
+    a++;
+}
+
+let a = 1;
+for (let i = 0; i < 100; i++){
+    console.log(a);
+    a++;
+}
--- js/script.js
+++ js/script.js
@@ -1,14 +1,122 @@
 'use strict';
 
-    let numberOfFilms = +prompt('How many films you watched?', '');
+let numberOfFilms;
+
+
+
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
+    movies: {},
+    actors: {},
+    genres: [],
     privat: false
 };
+
+
+
+
+function filmAndMarks() {
+    for (let i = 0; i < numberOfFilms; i++) {
+        let a;
+
+        for (let f1 = 0; f1 < 1; f1++) {
+            a = prompt('Last film?', '');
+            if (a == false) {
+                alert('Your answer is empty');
+                f1--;
+            } else if (a.length > 50) {
+                alert('Your answer is very long');
+                f1--;
+            } else {
+                personalMovieDB.movies[a];
+            }
+        }
+        let b;
+        for (let a1 = 0; a1 < 1; a1++) {
+            const b = prompt('Ocenka?', '');
+            if (b == false) {
+                alert('Your answer is empty');
+                a1--;
+            } else if (b.length > 50) {
+                alert('Your answer is very long');
+                a1--;
+            }
+            personalMovieDB.movies[a] = b;
+        }
+
+    }
+}
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("prosmotreno malo filmov");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Vy klassicheskiy zritel');
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Vy kinoman");
+    } else {
+        console.log('Error');
+    }
+}
+
+
+
+function start() {
+    numberOfFilms = +prompt('How many films you watched?', '');
+    personalMovieDB.count = numberOfFilms;
+
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many films you watched?', '');
+        personalMovieDB.count = numberOfFilms;
+    }
+}
+
+function showMyDB(durak) {
+    if (!durak) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`Your favorite genre №${i+1}? `, '');;
+    }
+}
+
+function privates (){
+    let asd = window.confirm('You allow to share your answers?');
+     if (asd == true) {
+         personalMovieDB.privat = false;
+     } else {
+         personalMovieDB.privat = true;
+     }
+ }
+
+
+
+ privates();
+ start();
+ filmAndMarks();
+ writeYourGenres();
+ detectPersonalLevel();
+ showMyDB(personalMovieDB.privat);
+
+
+
+
+
+
+
+/*let lastFilm = prompt('Last film?', ''),
+    ocenka = prompt('Ocanka', ''),
+    lastFilm1 = prompt('Last film?', ''),
+    ocenka1 = prompt('Ocanka', '');
+personalMovieDB.movies[lastFilm] = ocenka;
+personalMovieDB.movies[lastFilm1] = ocenka1;
+console.log(personalMovieDB.movies);*/
+
+
 
 /*for(let i = 0; i < numberOfFilms; i++) {
     const a = prompt('Last film?', ''),
@@ -19,52 +127,3 @@ const personalMovieDB = {
         i--;
     }
 }*/
-
-for(let i = 0; i < numberOfFilms; i++){
-    let a;
-    
-    for(let f1 = 0; f1 < 1; f1++){
-        a = prompt('Last film?', '');
-        if (a == false) {
-            alert('Your answer is empty');
-            f1--;
-        } else if (a.length > 50) {
-            alert('Your answer is very long');
-            f1--;
-        } else {
-            personalMovieDB.movies[a];
-        }
-    }
-    let b;
-    for(let a1 = 0; a1 < 1; a1++){
-        const b = prompt('Ocenka?', '');
-        if (b == false) {
-            alert('Your answer is empty');
-            a1--;
-        } else if (b.length > 50) {
-            alert('Your answer is very long');
-            a1--;
-        }
-        personalMovieDB.movies[a] = b;
-    }
-    
-}
-if(personalMovieDB.count <10) {
-    console.log("prosmotreno malo filmov");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Vy klassicheskiy zritel');
-} else if (personalMovieDB.count >= 30){
-    console.log("Vy kinoman");
-} else {
-    console.log('Error');
-}
-
-console.log(personalMovieDB);
-
-/*let lastFilm = prompt('Last film?', ''),
-    ocenka = prompt('Ocanka', ''),
-    lastFilm1 = prompt('Last film?', ''),
-    ocenka1 = prompt('Ocanka', '');
-personalMovieDB.movies[lastFilm] = ocenka;
-personalMovieDB.movies[lastFilm1] = ocenka1;
-console.log(personalMovieDB.movies);*/
