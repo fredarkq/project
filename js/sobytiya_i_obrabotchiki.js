a8c96696d6921dc6f115be2ea67bb95c450d5c35
Eugen <fredarkq18@gmail.com>
2021-12-09 12:59:21 +0200

    Sobytyia_i_obrabotka

---
+++ js/sobytiya_i_obrabotchiki.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const btns = document.querySelectorAll('button'),
+      overlay = document.querySelector('.overlay');
+
+// btn.onclick = function() {
+//     alert("DalbaEb");
+// };
+
+// btn.addEventListener('click', () => {
+//     alert('click');
+// });
+
+let i = 0;
+const deleteElement = (e) => {
+    console.log(e.currentTarget);
+    console.log(e.type);
+    // i++;
+    // if (i == 2) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }
+};
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+btns.forEach(btn => {
+    btn.addEventListener('click', deleteElement, {once: true});
+});
+
+const link = document.querySelector('a');
+
+link.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(event.target);
+});
