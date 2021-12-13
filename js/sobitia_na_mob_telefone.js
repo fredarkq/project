4b30c1f5f225b74a02475448c5c4ec8bd674db52
Eugen <fredarkq18@gmail.com>
2021-12-13 16:46:20 +0200

    sobitiya na telefone, classlist, callback

---
+++ js/sobitia_na_mob_telefone.js
@@ -0,0 +1,32 @@
+"use-strict"
+// // tochstart
+// // touchmove
+// // touchend
+// // touchenter
+// // touchleave
+// // touchcancel
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const box = document.querySelector('.box');
+
+//     // box.addEventListener('touchstart', (event) => {
+//     //     event.preventDefault();
+//     //     console.log("start");
+//     //     console.log(event.changedTouches);
+//     // });
+//     box.addEventListener('touchmove', (event) => {
+//         event.preventDefault();
+//         console.log(event.targetTouches[0].pageX);
+//     });
+//     // box.addEventListener('touchend', (event) => {
+//     //     event.preventDefault();
+//     //     console.log("end");
+//     // });
+// });
+
+// // touches
+// // targetTouches
+// // changedTouches
+
+const p = document.querySelectorAll('p');
+console.log(p);
