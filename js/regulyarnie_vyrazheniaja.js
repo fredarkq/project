0e81f9946eadee7c7f3cae86b9757879a26e36e8
Eugen <fredarkq18@gmail.com>
2022-01-05 16:20:15 +0200

    new lessons

---
+++ js/regulyarnie_vyrazheniaja.js
@@ -0,0 +1,34 @@
+'use ctrict';
+
+// new RegExp('pattern', 'flags');
+// /pattern/
+
+// const ans = prompt('Write your age');
+
+// const reg = /\d/g;
+// console.log(ans.match(reg));
+
+const str = 'my name is R2D2';
+
+console.log(str.match(/\D/ig));
+
+//    \D - ne chisla  \W ne bukvi
+
+//   \d шукаємо цифри
+//   \w шукаємо слова
+//   \s шукаємо пробєли
+
+// i - незадежно від регістра, g, m;
+
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/./g, "*"));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
+
+
